0523beefa089af8f8741a77dd353c32b8d2f39e2
Francesc Zacarias <fzacarias3k@gmail.com>
2020-12-02 23:22:55 +0000

    solve problem 02 with python

---
+++ 02/passwd_policy.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import argparse
+import textwrap
+import sys
+from typing import Tuple
+
+
+class Policy:
+    def __init__(self, raw_str: str):
+        # Expected raw_str format:
+        #   "range character"
+        # Example:
+        #   "6-8 g"
+        num_range, char = raw_str.split(' ')
+        min_str, max_str = num_range.split('-')
+        self.min = int(min_str)
+        self.max = int(max_str)
+        self.char = char
+        if self.min >= self.max:
+            raise ValueError("Min must be smaller than Max")
+
+    def match_count(self, passwd: str) -> bool:
+        # Interpret the numbers in policy as a range.
+        # That's the number of occurrences that char is allowed to have in
+        # the password.
+        counter = 0
+        for c in passwd:
+            if c == self.char:
+                counter += 1
+        return counter >= self.min and counter <= self.max
+
+    def match_pos(self, passwd: str) -> bool:
+        # Interpret the numbers in policy as positions.
+        # The char must appear in one or the other, but not both
+        return (passwd[self.min-1] == self.char) != (passwd[self.max-1] == self.char)
+
+
+def parse_line(line: str) -> Tuple[Policy, str]:
+    # Expected line format:
+    #   range character: text
+    # Example:
+    #   4-6 v: vvvvvqvvv
+    polstr, passwd = line.split(': ')
+    return Policy(polstr), passwd
+
+
+def main():
+    aparser = argparse.ArgumentParser(
+        description=textwrap.dedent("""\
+        Read a list of policies and passwords from stdin and count how many
+        passwords match their policies."""))
+    aparser.add_argument("policy", choices=["count", "position"])
+    args = aparser.parse_args()
+    if args.policy == "count":
+        method_name = "match_count"
+    else:
+        method_name = "match_pos"
+
+    count = 0
+    for line in sys.stdin:
+        try:
+            policy, passwd = parse_line(line.strip())
+        except Exception:
+            print(f"Could not parse this line: {line}", file=sys.stderr)
+            sys.exit(1)
+        func = getattr(policy, method_name)
+        if func(passwd):
+            count += 1
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02/test_passwd_policy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import unittest
+import passwd_policy
+
+
+class TestParseLine(unittest.TestCase):
+    def test_parse_line(self):
+        pol, passwd = passwd_policy.parse_line("4-6 v: vvvvvqvvv")
+        self.assertEqual("vvvvvqvvv", passwd)
+        self.assertEqual(4, pol.min)
+        self.assertEqual(6, pol.max)
+        self.assertEqual("v", pol.char)
+
+
+class TestPasswdPolicyCount(unittest.TestCase):
+    def test_policy_no_match_too_many(self):
+        p = passwd_policy.Policy("4-6 v")
+        self.assertFalse(p.match_count("vvvvvqvvv"))
+
+    def test_policy_no_match_too_few(self):
+        p = passwd_policy.Policy("4-6 q")
+        self.assertFalse(p.match_count("vvvvvqvvv"))
+
+    def test_policy_match(self):
+        p = passwd_policy.Policy("4-6 v")
+        self.assertTrue(p.match_count("vvvqvvv"))
+
+
+class TestPasswdPolicyPosition(unittest.TestCase):
+    def test_policy_no_match(self):
+        p = passwd_policy.Policy("2-9 c")
+        self.assertFalse(p.match_pos("ccccccccc"))
+
+    def test_policy_match(self):
+        p = passwd_policy.Policy("1-3 a")
+        self.assertTrue(p.match_pos("abcde"))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
