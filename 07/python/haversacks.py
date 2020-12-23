a94094104ec2d8667f13deffea3bc65803619696
Francesc Zacarias <fzacarias3k@gmail.com>
2020-12-23 17:57:46 +0000

    solve problem 07 with python

---
+++ 07/python/haversacks.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import argparse
+from collections import defaultdict
+import re
+from typing import Dict, List, TextIO, Set, Tuple
+import sys
+
+Graph = Dict[str, List[Tuple[int, str]]]
+
+REGEX_NEIGH = r"(\d+) (\w+ \w+) bag"
+
+def parse_line(line: str) -> Tuple[str, List[Tuple[int, str]]]:
+    """
+    Parses one line of text with bag information. Examples:
+      "light red bags contain 1 bright white bag, 2 muted yellow bags."
+      "faded blue bags contain no other bags."
+    Returns the first bad identifier and a list with tuples, each containing
+    the number of contained bags and their identifieds.
+    For the examples above, this function would respectively return:
+      "light red", [(1, "bright white"), (2, "muted yellow")]
+      "faded blue", []
+    """
+    node, neigh_raw = line.split(" bags contain ")
+    if neigh_raw == "no other bags.":
+        return node, []
+    matches = re.findall(REGEX_NEIGH, neigh_raw)
+    parsed_matches = [(int(x), y) for x,y in matches]
+    return node, parsed_matches
+
+
+def build_graphs(in_stream: TextIO) -> Tuple[Graph, Graph]:
+    graph = defaultdict(list)
+    revgraph = defaultdict(list)
+    for line in in_stream:
+        line = line.strip()
+        node, neighs = parse_line(line)
+        graph[node] = neighs
+        for ne in neighs:
+            revgraph[ne[1]].append((ne[0], node))
+    return graph, revgraph
+
+
+def bfs(graph: Graph, start: str) -> Set[str]:
+    visited = set()
+    pending = [start]
+    while pending:
+        new_pending = []
+        for n in pending:
+            for nn in graph[n]:
+                nn_name = nn[1]
+                if nn_name not in visited:
+                    new_pending.append(nn_name)
+            visited.add(n)
+        pending = new_pending
+    return visited
+
+
+def count_bags(graph: Graph, start: str) -> int:
+    total = 0
+    neighs = graph[start]
+    if not neighs:
+        return 0
+    for neigh in neighs:
+        total += neigh[0] + (neigh[0] * count_bags(graph, neigh[1]))
+    return total
+
+
+def main():
+    aparser = argparse.ArgumentParser()
+    aparser.add_argument("problem", choices=["one", "two"])
+    args = aparser.parse_args()
+    graph, revgraph = build_graphs(sys.stdin)
+    if args.problem == "one":
+        print(len(bfs(revgraph, "shiny gold")) - 1)
+    else:
+        print(count_bags(graph, "shiny gold"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 07/python/test_haversacks.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import io
+import haversacks
+import textwrap
+import unittest
+
+class TestHaversacksParseLine(unittest.TestCase):
+    def test_no_other_bags(self):
+        node, neigh = haversacks.parse_line("dotted black bags contain no other bags.")
+        self.assertEqual("dotted black", node)
+        self.assertEqual([], neigh)
+
+    def test_match_two_bags(self):
+        node, neigh = haversacks.parse_line("light red bags contain 1 bright white bag, 2 muted yellow bags.")
+        self.assertEqual("light red", node)
+        self.assertEqual([(1, "bright white"), (2, "muted yellow")], neigh)
+
+
+class TestHaversacksOne(unittest.TestCase):
+    def setUp(self):
+        self.text = io.StringIO(textwrap.dedent("""\
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags."""))
+
+    def test_build_graphs(self):
+        graph, revgraph = haversacks.build_graphs(self.text)
+        self.assertEqual(9, len(graph))
+        self.assertEqual(7, len(revgraph))
+
+    def test_example_one(self):
+        _, revgraph = haversacks.build_graphs(self.text)
+        self.assertEqual(5, len(haversacks.bfs(revgraph, "shiny gold")))
+
+    def test_example_two(self):
+        graph, _ = haversacks.build_graphs(self.text)
+        self.assertEqual(32, haversacks.count_bags(graph, "shiny gold"))
+
+class TestHaversacksTwo(unittest.TestCase):
+    def setUp(self):
+        self.text = io.StringIO(textwrap.dedent("""\
+		shiny gold bags contain 2 dark red bags.
+		dark red bags contain 2 dark orange bags.
+		dark orange bags contain 2 dark yellow bags.
+		dark yellow bags contain 2 dark green bags.
+		dark green bags contain 2 dark blue bags.
+		dark blue bags contain 2 dark violet bags.
+		dark violet bags contain no other bags."""))
+
+    def test_example_two_extra(self):
+        graph, _ = haversacks.build_graphs(self.text)
+        self.assertEqual(126, haversacks.count_bags(graph, "shiny gold"))
+
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
